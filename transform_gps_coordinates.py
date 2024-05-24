3aa0862448441f65f6be256120cf4464bfccd4d2
mirkavychopnova <vychopnova.mirka@gmail.com>
2024-05-24 18:38:11 +0200

    added logger

--- transform_gps_coordinates.py
+++ transform_gps_coordinates.py
@@ -1,5 +1,10 @@
 import json
+import logging
 from pyproj import Proj, transform
+
+# Konfigurace loggeru
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
 # Definuj projekci pro vstupní (zeměpisné) souřadnice
 input_proj = Proj(init='epsg:4326')  # WGS 84 (zeměpisný)
@@ -8,10 +13,13 @@ input_proj = Proj(init='epsg:4326')  # WGS 84 (zeměpisný)
 output_proj = Proj(init='epsg:3857')  # WGS 84 / UTM zone 33N
 
 # Načíst GeoJSON soubor
-with open('/Users/mirus/Desktop/python/PROJEKT/python-projekt/souradnice/teplota_nespojita.geojson', 'r') as f:
+logger.info("Načítání GeoJSON souboru.")
+with open('/Users/mirus/Desktop/python/PROJEKT/python-projekt/souradnice/kvalita_ovzdusi.geojson', 'r') as f:
     data = json.load(f)
+logger.info("GeoJSON soubor načten.")
 
 # Pro každou vlastnost v GeoJSON souboru
+logger.info("Převod souřadnic zahájen.")
 for feature in data['features']:
     # Získání zeměpisných souřadnic
     longitude, latitude = feature['geometry']['coordinates']
@@ -21,7 +29,10 @@ for feature in data['features']:
     
     # Nahraď původní zeměpisné souřadnice novými projektovanými souřadnicemi
     feature['geometry']['coordinates'] = [x, y]
+logger.info("Převod souřadnic ukončen.")
 
 # Ulož upravený GeoJSON soubor
-with open('/Users/mirus/Desktop/python/PROJEKT/python-projekt/souradnice/teplota_nespojita_prevedena.geojson', 'w') as f:
+logger.info("Ukládání nového souboru.")
+with open('/Users/mirus/Desktop/python/PROJEKT/python-projekt/souradnice/kvalita_ovzdusi_prevedena.geojson', 'w') as f:
     json.dump(data, f, indent=4)
+logger.info("Nový soubor uložen.")
